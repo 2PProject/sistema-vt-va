78b660cafbbfa7ce70d06d39dc09f11f02731769
2PProject <164066179+2PProject@users.noreply.github.com>
2026-03-11 14:40:57 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
-    <div>
+    <main style={{padding:40,fontFamily:"Arial"}}>
       <h1>Sistema de Gestão VT / VA</h1>
       <p>Grupo Meire Reis</p>
-    </div>
-  );
+
+      <ul>
+        <li>Empresas</li>
+        <li>Unidades</li>
+        <li>Funcionários</li>
+        <li>Competências</li>
+        <li>Recibos</li>
+      </ul>
+    </main>
+  )
 }
